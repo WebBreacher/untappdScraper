b2a6e7093b4a4b5cc3c360906808f9a36fa4c549
WebBreacher <WebBreacher@users.noreply.github.com>
2018-10-15 19:37:31 -0400

    adding shell geocode_api_key file

---
+++ geocode_api_keys.py
@@ -0,0 +1,4 @@
+# You need to get one or more of these API keys from Google and Bing
+# The Google one may cost money. Bing is free.
+google_api_key = 'GOOGLE_API_KEY'
+bing_api_key = 'BING_API_KEY'
