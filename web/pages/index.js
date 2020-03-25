8a6569fa79c50a88bed5dfb6a0d825f3933b1037
weslley braga <wesbragagt@gmail.com>
2020-03-24 23:11:08 -0500

    basic navbar

--- web/pages/index.js
+++ web/pages/index.js
@@ -143,6 +143,28 @@ export default class Index extends Component {
   render () {
     return (
       <div className="container">
+        <menu className="navbar-menu">
+          <div id="logoImg"></div>
+          <div id="userForm">
+            <form onSubmit={e => { this.getUntappdOsint(e) }} disabled={this.state.submitting}>
+              <input className="text-input" placeholder="Username" type="text" value={this.state.username} onChange={e => { this.updateUsername(e) }} />
+              <label>Recent Activity <strong>only</strong></label>
+              <input type="checkbox" value={this.state.recentActivityOnly} onChange={e => { this.updateRecentActivityOnly(e) }} />
+              <input className="button" type="submit" value="Scrape" disabled={this.state.submitting} />
+            </form>
+          </div>
+          <div id="mapsAPIForm">
+            <form onSubmit={e => { this.setupGoogleMapsClient(e) }}>
+              {/* <p>Optionally, you can provide a valid Google Maps API key, which will enable the application to analyze the locations it finds. This key is never sent to any server other than the Google Maps API.</p> */}
+
+               <input className="text-input" placeholder="Google Maps API Key" type="password" value={this.state.googleMapsApiKey} onChange={e => { this.updateGoogleMapsApiKey(e) }} readOnly={this.state.googleMapsClient} />
+
+              <input className="button" type="submit" value="Set" disabled={this.state.loadingGoogleMapsClient || this.state.googleMapsClient} />
+
+              <p><em>Note: This might silently fail if the API key provided is invalid or for an account that does not have the Maps JavaScript API and Geocoding APIs enabled. Check the developer console if the Maps functionality does not work correctly after setting the API key.</em></p>
+            </form>
+          </div>
+        </menu>
         {this.state.error &&
           <strong>{this.state.error}</strong>
         }
@@ -352,22 +374,7 @@ export default class Index extends Component {
         <div style={{ height: (this.state.data && this.state.data.map) ? '400px' : 0 }}>
           <div id="map"></div>
         </div>
-
-        <form onSubmit={e => { this.getUntappdOsint(e) }} disabled={this.state.submitting}>
-          <p>Username: <input type="text" value={this.state.username} onChange={e => { this.updateUsername(e) }} /></p>
-          <p>Recent Activity Only: <input type="checkbox" value={this.state.recentActivityOnly} onChange={e => { this.updateRecentActivityOnly(e) }} /></p>
-          <input type="submit" value="Get Untappd OSINT" disabled={this.state.submitting} />
-        </form>
-
-        <form onSubmit={e => { this.setupGoogleMapsClient(e) }}>
-          <p>Optionally, you can provide a valid Google Maps API key, which will enable the application to analyze the locations it finds. This key is never sent to any server other than the Google Maps API.</p>
-
-          <p>Google Maps API Key: <input type="password" value={this.state.googleMapsApiKey} onChange={e => { this.updateGoogleMapsApiKey(e) }} readOnly={this.state.googleMapsClient} /></p>
-
-          <input type="submit" value="Set Google Maps API Key" disabled={this.state.loadingGoogleMapsClient || this.state.googleMapsClient} />
-
-          <p><em>Note: This might silently fail if the API key provided is invalid or for an account that does not have the Maps JavaScript API and Geocoding APIs enabled. Check the developer console if the Maps functionality does not work correctly after setting the API key.</em></p>
-        </form>
+        
       </div>
     )
   }
