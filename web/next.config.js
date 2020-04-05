bf488488fa722d69812ce3d2030c2452b834a74b
Brandon Evans <contact@brandonmevans.com>
2020-04-04 23:35:27 -0500

    Deploy to GH pages.

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,13 @@
-// next.config.js
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withImages({
+const debug = process.env.NODE_ENV !== 'production';
+
+const config = withCSS(withImages({
   webpack (config, options) {
     return config
   }
 }))
+
+config.assetPrefix = !debug ? '/untappdScraper/' : '',
+
+module.exports = config
