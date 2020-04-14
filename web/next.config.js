6782bd0088c133a9aafbdc7eec5e7175832b02ba
Brandon Evans <contact@brandonmevans.com>
2020-04-14 11:37:33 -0500

    Remote asset prefix.

--- web/next.config.js
+++ web/next.config.js
@@ -1,16 +1,10 @@
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
-const debug = process.env.NODE_ENV !== 'production';
-
-const assetPrefix = !debug ? '/' : ''
 
 const config = withCSS(withImages({
-  assetPrefix,
   webpack (config, options) {
     return config
   }
 }))
-
-config.assetPrefix = assetPrefix
 
 module.exports = config
