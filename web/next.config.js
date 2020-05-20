c71b3d701aa14e7dad29ca7a5659a8d7401e11d6
Brandon Evans <contact@brandonmevans.com>
2020-05-20 14:59:39 -0500

    Fixed deployment.

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,13 @@
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
+const debug = process.env.NODE_ENV !== 'production'
 
 const config = withCSS(withImages({
   webpack (config, options) {
     return config
   }
 }))
+
+config.assetPrefix = !debug ? '/untappdScraper/' : ''
 
 module.exports = config
